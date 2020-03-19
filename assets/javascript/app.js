3008cc62d3108d8adf60c326e8547df0ad131735
Anibal <guerra15anibal@hotmail.com>
2020-03-19 09:19:22 -0600

    Changing API to something better

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,56 @@
 // Your web app's Firebase configuration
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC7BfOPJId6bXHFz1Q1Y-p8POWRrIguRrk",
-    authDomain: "safety-check-first.firebaseapp.com",
-    databaseURL: "https://safety-check-first.firebaseio.com",
-    projectId: "safety-check-first",
-    storageBucket: "safety-check-first.appspot.com",
-    messagingSenderId: "810661652634",
-    appId: "1:810661652634:web:1af75d7de6b2e238dd1723"
+  apiKey: "AIzaSyC7BfOPJId6bXHFz1Q1Y-p8POWRrIguRrk",
+  authDomain: "safety-check-first.firebaseapp.com",
+  databaseURL: "https://safety-check-first.firebaseio.com",
+  projectId: "safety-check-first",
+  storageBucket: "safety-check-first.appspot.com",
+  messagingSenderId: "810661652634",
+  appId: "1:810661652634:web:1af75d7de6b2e238dd1723"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+//Skybio API
 var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://faceplusplus-faceplusplus.p.rapidapi.com/facepp/v3/detect?return_attributes=hat%252C%20glasses&image_url=https%253A%252F%252Fcnet3.cbsistatic.com%252Fimg%252Fgk7d6AQXuqmtPNmnZI2gMaNySyA%253D%252F970x0%252F2018%252F09%252F05%252F7274da05-85a9-4646-b41f-a4b22c597507%252Fcaptain-marvel-brie-larson-1.jpg",
-    "method": "POST",
-    "headers": {
-        "x-rapidapi-host": "faceplusplus-faceplusplus.p.rapidapi.com",
-        "x-rapidapi-key": "d91fdc82admsh973e6e584b03779p178c1ajsn4063b76d234e",
-        "content-type": "application/x-www-form-urlencoded"
-    },
-    "data": {}
-}
+  async: true,
+  crossDomain: true,
+  url: "https://face.p.rapidapi.com/account/limits",
+  method: "POST",
+  headers: {
+    "x-rapidapi-host": "face.p.rapidapi.com",
+    "x-rapidapi-key": "3e9420af83msh9f3c3a2f99bf11bp154afejsn314b780e0306",
+    "content-type": "application/x-www-form-urlencoded"
+  },
+  data: {}
+};
+
 $.ajax(settings).done(function(response) {
-    console.log(response);
+  console.log(response);
+});
+
+//Giphy API
+var thing = $(this).attr("data");
+// console.log("thing: " + thing);
+var queryURL =
+  "https://api.giphy.com/v1/gifs/search?q=green+light&api_key=C01R2xURFqCz6oEkz89pIqaDGFPgxD4N&limit=1";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+});
+
+$(document).on("click", ".giphy-img", function() {
+  console.log("on click");
+  var state = $(this).attr("state");
+  if (state == "still") {
+    var animatedUrl = $(this).attr("data-animate");
+    $(this).attr("src", animatedUrl);
+    $(this).attr("state", "animated");
+  }
+  var stillUrl = $(this).attr("data-still");
+  $(this).attr("src", stillUrl);
+  $(this).attr("state", "still");
 });
